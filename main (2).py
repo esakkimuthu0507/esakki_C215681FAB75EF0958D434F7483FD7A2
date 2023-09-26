013836b3c4410268314c8fa4f8a6f299b614a452
esakkimuthu0507 <144409773+esakkimuthu0507@users.noreply.github.com>
2023-09-26 22:42:26 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,13 +1,23 @@
-year = int(input("Enter a year: "))
-if (year % 4) == 0:
- if (year% 100) == 0:
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
 
-   if (year % 400) == 0:
-     print("{0} is a leap year".format(year))
+def sort_students(student_list):
+    sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
+    return sorted_students
 
-   else:
-       print("{0} is not a leap year".format(year))
- else:
-     print("{0} is a leap year".format(year))
-else:
-  print("{0} is not a leap year".format(year))
+# Example usage:
+student1 = Student("Alice", "S123", 3.7)
+student2 = Student("Bob", "S124", 3.9)
+student3 = Student("Charlie", "S125", 3.5)
+student4 = Student("David", "S126", 3.8)
+
+students = [student1, student2, student3, student4]
+
+sorted_students = sort_students(students)
+
+# Print the sorted list of students by CGPA in descending order
+for student in sorted_students:
+    print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
